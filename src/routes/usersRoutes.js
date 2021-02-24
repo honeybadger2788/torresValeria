173b81105cb274a1bf1ab8ff496781d48595148c
unknown <noe.carosella@gmail.com>
2021-02-24 18:54:14 -0300

    mas validaciones

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult, body } = require('express-validator');
 const usersController = require ('../controllers/usersController');
 
 router.get ('/index',usersController.index);
 
 router.get ('/gastos',usersController.gastos);
-router.post ('/cargaGastos',usersController.cargarGasto);
+router.post ('/cargaGastos',[
+    check('fechaGasto')
+        .notEmpty(),
+    check('concepto')
+        .notEmpty(),
+    check('importe')
+        .notEmpty()
+        .isDecimal({force_decimal: false, decimal_digits: '2', locale: 'en-US'}),
+],usersController.cargarGasto);
 
 router.get ('/estadias',usersController.estadias);
 router.post ('/cargaEstadias',usersController.cargarEstadia);

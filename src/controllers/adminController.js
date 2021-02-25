4e823f1ff31d9b39e0c337576f04f7bc7f077a9b
honeybadger2788 <noe.carosella@gmail.com>
2021-02-25 17:38:55 -0300

    validando errores

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,39 +1,45 @@
 const bcript = require ('bcrypt');
+const { validationResult } = require('express-validator');
 const db = require ('../database/models/index');
 
 module.exports = {
     register: function (req,res){
+        let errors = validationResult(req);
         res.render('./admin/register',{
-            errors: undefined,
+            errors
         });
     },
     crearCuenta: function (req,res){
-        if (req.body.password == req.body.rePassword){
+        let errors = validationResult(req);
+        if (errors.isEmpty()){
             db.Usuario.findOrCreate({
-                where: {
-                    usuario: req.body.user,
-                },
-                defaults: {
-                    password: bcript.hashSync(req.body.password, 12),
-                }
-            })
-            .then (function(resultado){
-                if(resultado){
-                    return res.render('./admin/register', {
-                        errors: [
-                            {msg: "Usuario ya existente"},
-                        ]
-                    })
-                } else {
-                    return res.redirect('/')
-                }
-            });
+                    where: {
+                        usuario: req.body.user,
+                    },
+                    defaults: {
+                        password: bcript.hashSync(req.body.password, 12),
+                    }
+                })
+                .then (function(resultado){
+                    if(resultado){
+                        return res.render('./admin/register', {
+                            errors: [ 
+                                {
+                                errors:
+                                    {msg: "Usuario ya existente"},
+                                }
+                            ]
+                        })
+                    } else {
+                        return res.redirect('/')
+                    }
+                });
         } else {
             return res.render('./admin/register', {
-                errors:  [
-                    {msg: "Las contraseñas no coinciden"},
-                ]
+                errors:  errors.errors
             })
         }
+        
     }
 }
+
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,23 +1,25 @@
 const express = require ("express");
 const router = express.Router();
-const { check, validationResult, body } = require('express-validator');
+const { check, body } = require('express-validator');
 const adminController = require ('../controllers/adminController');
 
 router.get ('/register',adminController.register);
 router.post ('/register',[
-    check('usuario').isEmail(),
+    check('user').isEmail().withMessage('El usuario debe ser un email'),
     check('password')
     .notEmpty()
     .isLength({
       min: 8
     })
-    .custom(() => {
+    .withMessage("Las contraseña debe contener al menos 8 caracteres"),
+    body('password').custom(() => {
       if (req.body.password === req.body.rePassword) {
         return true;
       } else {
         return false;
       }
-    }),
+    })
+    .withMessage("Las contraseñas no coinciden"), //NO FUNCIONA
 ],adminController.crearCuenta);
 
 module.exports = router;
